fb62aae079f74e036bfaf7a5c8e8c4b135b07492
= <675973885@qq.com>
2016-08-15 22:15:34 -0400

    Adding all operator precedence and associativity

--- src/token.cpp
+++ src/token.cpp
@@ -38,3 +38,124 @@ const TokenInfo::keyword_map_type TokenInfo::keyword_map {
   	TokenInfo::keyword_map_value_type{"static", TokenType::T_STATIC},
   }
 };
+
+// Operator precedence and associativity map
+const TokenInfo::op_map_type TokenInfo::op_map {
+  {
+    // precedence = 2
+  	TokenInfo::op_map_value_type{TokenType::T_POST_INC,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_POST_DEC,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_FUNCCALL,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_ARRAYSUB,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_DOT,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_ARROW,
+                                 std::make_pair(2, EvalOrder::LEFT_TO_RIGHT)},
+  	
+  	// precedence = 3
+  	TokenInfo::op_map_value_type{TokenType::T_PRE_INC,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_PRE_DEC,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_POS,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_NEG,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_NOT,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_BITNOT,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_TYPECAST,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_DEREF,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_ADDR,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_SIZEOF,
+                                 std::make_pair(3, EvalOrder::RIGHT_TO_LEFT)},
+                                 
+    // Precedence 5
+    TokenInfo::op_map_value_type{TokenType::T_MULT,
+                                 std::make_pair(5, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_DIV,
+                                 std::make_pair(5, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_MOD,
+                                 std::make_pair(5, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 6
+    TokenInfo::op_map_value_type{TokenType::T_ADDITION,
+                                 std::make_pair(6, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_SUBTRACTION,
+                                 std::make_pair(6, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 7
+    TokenInfo::op_map_value_type{TokenType::T_LSHIFT,
+                                 std::make_pair(7, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_RSHIFT,
+                                 std::make_pair(7, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 8
+    TokenInfo::op_map_value_type{TokenType::T_LESS,
+                                 std::make_pair(8, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_LESSEQ,
+                                 std::make_pair(8, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_GREATER,
+                                 std::make_pair(8, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_GREATEREQ,
+                                 std::make_pair(8, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 9
+    TokenInfo::op_map_value_type{TokenType::T_EQ,
+                                 std::make_pair(9, EvalOrder::LEFT_TO_RIGHT)},
+    TokenInfo::op_map_value_type{TokenType::T_NOTEQ,
+                                 std::make_pair(9, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 10
+    TokenInfo::op_map_value_type{TokenType::T_BITAND,
+                                 std::make_pair(10, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 11
+    TokenInfo::op_map_value_type{TokenType::T_BITXOR,
+                                 std::make_pair(11, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 12
+    TokenInfo::op_map_value_type{TokenType::T_BITOR,
+                                 std::make_pair(12, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 13
+    TokenInfo::op_map_value_type{TokenType::T_AND,
+                                 std::make_pair(13, EvalOrder::LEFT_TO_RIGHT)},
+    
+    // Precedence 14
+    TokenInfo::op_map_value_type{TokenType::T_OR,
+                                 std::make_pair(14, EvalOrder::LEFT_TO_RIGHT)},
+                                 
+    // Precedence 16
+    TokenInfo::op_map_value_type{TokenType::T_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_PLUS_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_MINUS_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_STAR_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_DIV_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_MOD_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_LSHIFT_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_RSHIFT_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_AMPERSAND_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_BITXOR_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+    TokenInfo::op_map_value_type{TokenType::T_BITOR_ASSIGN,
+                                 std::make_pair(16, EvalOrder::RIGHT_TO_LEFT)},
+  }
+};
--- src/token.h
+++ src/token.h
@@ -4,6 +4,7 @@
 #include <utility>
 #include <unordered_map>
 #include <string>
+#include <functional>
 
 enum class TokenType {
   // The following is keyword types
@@ -54,7 +55,7 @@ enum class TokenType {
 	
 	T_WHILE,
 
-	// The following are operator types
+	// The following are primitive operator types
 	
 	T_INC = 100,
 	T_DEC,
@@ -69,7 +70,7 @@ enum class TokenType {
   T_PLUS,
   T_MINUS,
   T_NOT,
-  T_BIT_NOT,
+  T_BITNOT,
   T_STAR,
   T_AMPERSAND,
   T_DIV,
@@ -104,11 +105,77 @@ enum class TokenType {
   T_BITXOR_ASSIGN,
   T_BITOR_ASSIGN,
   T_SIZEOF,
+  
+  // The following are operator types for overloading in C
+  // e.g. ++ and -- have pre- and post-fix form
+  
+  // ++
+  T_POST_INC = 200,
+  T_PRE_INC,
+  
+  // --
+  T_POST_DEC,
+  T_PRE_DEC,
+
+  // *
+  T_MULT,
+  T_DEREF,
+
+  // &
+  T_ADDR,
+  T_BITAND,
+  
+  // -
+  T_NEG,
+  T_SUBTRACTION,
+  
+  // +
+  T_POS,
+  T_ADDITION,
+  
+  // ()
+  T_FUNCCALL,
+  // []
+  T_ARRAYSUB,
+  // (TYPE)
+  T_TYPECAST,
+  
+};
+
+// This defines the evaluation order of operators in the same
+// precedence level
+// i.e. associativity
+enum class EvalOrder {
+  LEFT_TO_RIGHT = 0,
+  RIGHT_TO_LEFT,
+};
+
+struct TokenTypeHasher {
+  inline size_t operator()(const TokenType &tt) const {
+    return static_cast<size_t>(tt);
+  }
+};
+
+struct TokenTypeEq {
+  inline bool operator()(const TokenType &tt1, const TokenType &tt2) const {
+    return static_cast<int>(tt1) == static_cast<int>(tt2);
+  }
 };
 
 class TokenInfo {
   using keyword_map_value_type = std::pair<std::string, TokenType>;
   using keyword_map_type = std::unordered_map<std::string, TokenType>;
 
+  // The value type used in operator map
+  using op_map_value_type = \
+    std::pair<TokenType, std::pair<int, EvalOrder>>;
+    
+  using op_map_type = \
+    std::unordered_map<TokenType,
+                       std::pair<int, EvalOrder>,
+                       TokenTypeHasher,
+                       TokenTypeEq>;
+
   static const keyword_map_type keyword_map;
+  static const op_map_type op_map;
 };
