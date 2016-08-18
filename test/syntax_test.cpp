a7968be4ad4f11b5e7333c70d1ff63572737589d
Ziqi Wang <675973885@qq.com>
2016-08-18 03:25:46 -0400

    Adding new test file for syntax analyzer

---
+++ test/syntax_test.cpp
@@ -0,0 +1,23 @@
+
+#include "test_suite.h"
+
+void TestParseExpression1() {
+  printf("========== TestParseExpression1 ==========\n");
+  
+  char data[] = "123456 + a";
+
+  printf("Test string: %s\n", data);
+
+  SourceFile sf{data, sizeof(data) - 1};
+  SyntaxAnalyzer sa{&sf};
+  
+  try {
+    SyntaxNode *node_p = sa.ParseExpression();
+    
+    node_p->TraversePrint();
+  } catch(const std::string &reason) {
+    std::cout << reason << std::endl;
+
+    return;
+  }
+}
