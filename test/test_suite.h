be23ec1ca3a4de7688a6d9e3bb0c660b40d70bc7
= <675973885@qq.com>
2016-08-24 18:31:45 -0400

    Adding using namespace into test header file

--- test/test_suite.h
+++ test/test_suite.h
@@ -12,6 +12,10 @@
 #include "../src/token.h"
 #include "../src/syntax.h"
 
+// It is safe to put it here since we only use the header file for testing
+using namespace wangziqi2013;
+using namespace cfront;
+
 std::string AppendDoubleQuote(std::string s);
 
 void TestLexClipCharLiteral();
