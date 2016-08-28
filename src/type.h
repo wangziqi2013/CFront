b75f1b338b8ddefc6388ce1f70df84b1bfee9936
= <675973885@qq.com>
2016-08-27 20:54:46 -0400

    Adding include common.h

--- src/type.h
+++ src/type.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include "common.h"
 #include <vector>
 
 namespace wangziqi2013 {
@@ -32,7 +33,39 @@ enum class TypeNodeType {
  *                  type information from other types
  */
 class TypeNode {
+ private:
+  // This is the type of nodes - could either be primitive types or
+  // compound types
   TypeNodeType type;
+  
+  
+  union {
+    // The length of bits if it is an integer type
+    int bit_width;
+    
+    // Holds children nodes if it is a composite type
+  };
+  
+ public:
+
+  /*
+   * GetType() - Returns the type of the node
+   */
+  TypeNodeType GetType() const {
+    return type;
+  }
+  
+  /*
+   * GetBitWidth() - Returns the number of bits inside the integer type
+   *
+   * We check whether the node is of INTEGER_T type. If not then assertion
+   * fails
+   */
+  int GetBitWidth() const {
+    assert(type == TypeNodeType::INTEGER_T);
+    
+    return bit_width;
+  }
 };
 
 } // namespace cfront
