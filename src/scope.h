c26e2180678bbae4d9d598fc92a54ef054718a4a
= <675973885@qq.com>
2016-09-04 15:14:35 -0400

    Adding scope class member definition

--- src/scope.h
+++ src/scope.h
@@ -1,7 +1,10 @@
 
 #pragma once
 
-#include <unordered_map>
+#include "common.h"
+
+namespace wangziqi2013 {
+namespace cfront {
 
 /*
  * class ScopeNode - This is a structure that contains information
@@ -19,9 +22,42 @@ class ScopeNode {
   // For the topmost level in this structure we should put 8 basic types:
   // char, short, int, long
   // unsigned char, unsigned short, unsigned int, unsigned long
-  std::unordered_map<std::string, SyntaxNode *> type_set;
+  std::unordered_map<std::string, SyntaxNode *> type_map;
   
   // TODO: Change the mapped type to something more meaningful
   // This should be
-  std::unordered_map<std::string, int> ident_set;
+  std::unordered_map<std::string, int> ident_map;
+ public:
+
+  /*
+   * Constructor - This is necessary for emplacing it back in a stack
+   */
+  ScopeNode() {}
+  
+  /*
+   * These are deleted to avoid any undesirable effects
+   */
+  ScopeNode(const ScopeNode &) = delete;
+  ScopeNode(ScopeNode &&) = delete;
+  ScopeNode &operator=(const ScopeNode &) = delete;
+  ScopeNode &operator=(ScopeNode &&) = delete;
+  
+  /*
+   * GetType() - Return the type node from type map
+   *
+   * If the type has not yet been defiend just return nullptr
+   */
+  SyntaxNode *GetType(const std::string &type_name) {
+    auto it = type_map.find(type_name);
+    
+    // If the type does not exist in the map just return nullptr
+    if(it == type_map.end()) {
+      return nullptr;
+    }
+    
+    return *it;
+  }
 };
+
+} // namespace wangziqi2013
+} // namespace cfront
