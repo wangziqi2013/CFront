b97063f90aba4bd60e2921c6520701740f527afd
= <675973885@qq.com>
2016-09-06 21:56:43 -0400

    Using vector instead of stack to support iterate

--- src/context.h
+++ src/context.h
@@ -14,7 +14,13 @@ namespace cfront {
  */
 class Context {
  private:
-  std::stack<ScopeNode> scope_stack;
+
+  // Note that we could not use stack here since stack does not support
+  // iteration, and during a search we have to iterate through the scope
+  // to search a named type
+  //
+  // Use push_back() and pop_back() to access elements like a stack
+  std::vector<ScopeNode> scope_stack;
 
  public:
 
@@ -35,9 +41,10 @@ class Context {
    *                and return the pushed object
    */
   ScopeNode &EnterScope() {
-    scope_stack.emplace();
+    // Construct an empty scope node and pusu it back to the vector
+    scope_stack.emplace_back();
     
-    return scope_stack.top();
+    return scope_stack.back();
   }
   
   /*
@@ -48,9 +55,34 @@ class Context {
   void LeaveScope() {
     assert(scope_stack.size() > 0);
     
-    scope_stack.pop();
+    scope_stack.pop_back();
     
     return;
+  }
+  
+  /*
+   * GetTypeNode() - Search on the stack for a named type
+   *
+   * This function searches the stack from the top top the bottom, and if
+   * the name exists inside any level that are searched first then it returns
+   * the associated type object
+   */
+  SyntaxNode *GetTypeNode(const std::string &type_name) {
+    // Iterate through the vector from high index to low index
+    // i.e. from most recent name space to less recent ones
+    for(auto it = scope_stack.rbegin(); it != scope_stack.rend();it++) {
+      SyntaxNode *type_node_p = it->GetTypeNode(type_name);
+      
+      // If the type exists in the scope being searched just return it
+      // Otherwise need to continue to the next scope
+      if(type_node_p != nullptr) {
+        return type_node_p;
+      }
+    }
+    
+    // If at last we did not find such name then the type does
+    // not exist and return nullptr
+    return nullptr;
   }
 };
 
