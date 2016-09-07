17750664d5bb431292f10b683d3695a8c9cefa41
= <675973885@qq.com>
2016-09-06 22:56:49 -0400

    Adding test for built in type parsing

--- test/syntax_test.cpp
+++ test/syntax_test.cpp
@@ -101,11 +101,55 @@ void TestParseExpression4() {
   }
 }
 
+/*
+ * TestParseType1() - Testing parsing signed and unsigned integral types
+ */
+void TestParseType1() {
+  printf("========== TestParseType1 ==========\n");
+
+  char data[] = "unsigned long     signed int    int  "
+                "   unsigned char char signed short ";
+
+  printf("Test string: %s\n", data);
+
+  SourceFile sf{data, sizeof(data) - 1};
+  Context context{};
+  TypeParser ta{&sf, &context};
+
+  try {
+    SyntaxNode *node_p = nullptr;
+    
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_ULONG);
+
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_INT);
+    
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_INT);
+    
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_UCHAR);
+    
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_CHAR);
+    
+    node_p = ta.TryParseBaseType();
+    assert(node_p->GetType() == TokenType::T_SHORT);
+  } catch(const std::string &reason) {
+    std::cout << reason << std::endl;
+
+    return;
+  }
+}
+
 int main() {
   TestParseExpression1();
   TestParseExpression2();
   TestParseExpression3();
   TestParseExpression4();
+  
+  TestParseType1();
   
   return 0;
 }
