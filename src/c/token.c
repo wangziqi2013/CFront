e528cbeb98c7e94092dde87f639138ef7a5cac66
Ziqi Wang <ziqiw@mit.edu>
2018-08-31 19:50:28 -0400

    Finish minus

--- src/c/token.c
+++ src/c/token.c
@@ -6,17 +6,27 @@
 char *token_get_op(char *s, token_t *token) {
   assert(*s != '\0');
   switch(s[0]) {
-    case ',': token->type = T_COMMA; return s + 1;
-    case '(': token->type = T_LCPAREN; return s + 1;
-    case ')': token->type = T_RCPAREN; return s + 1;
-    case '[': token->type = T_LSPAREN; return s + 1;
-    case ']': token->type = T_RSPAREN; return s + 1;
-    case '{': token->type = T_LCPAREN; return s + 1;
-    case '}': token->type = T_RCPAREN; return s + 1;
-    case '.': token->type = T_DOT; return s + 1;
-    case '?': token->type = T_QMARK; return s + 1;
-    case ':': token->type = T_COLON; return s + 1;
-    case '~': token->type = T_BIT_COMPLETEMT; return s + 1;
-    default: assert(0);
+    // Must be single character operator
+    case ',': token->type = T_COMMA; return s + 1;                      // ,
+    case '(': token->type = T_LCPAREN; return s + 1;                    // (
+    case ')': token->type = T_RCPAREN; return s + 1;                    // )
+    case '[': token->type = T_LSPAREN; return s + 1;                    // [
+    case ']': token->type = T_RSPAREN; return s + 1;                    // ]
+    case '{': token->type = T_LCPAREN; return s + 1;                    // {
+    case '}': token->type = T_RCPAREN; return s + 1;                    // }
+    case '.': token->type = T_DOT; return s + 1;                        // .
+    case '?': token->type = T_QMARK; return s + 1;                      // ?
+    case ':': token->type = T_COLON; return s + 1;                      // :
+    case '~': token->type = T_BIT_COMPLETEMT; return s + 1;             // ~
+    // Multi character
+    case '-':
+      switch(s[1]) {
+        case '-': token->type = T_DEC; return s + 2;                    // --
+        case '=': token->type = T_MINUS_ASSIGN; return s + 2;           // -=
+        case '>': token->type = T_ARROW; return s + 2;                  // ->
+        case '\0': 
+        default: token->type = T_MINUS; return s + 1;                   // -
+      }
+    case '+'
   }
 }
