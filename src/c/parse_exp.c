00b3fa8246d3e0504150a8be33db649684522d05
Ziqi Wang <ziqiw@mit.edu>
2018-08-31 18:23:36 -0400

    Adding parse exp context

--- src/c/parse_exp.c
+++ src/c/parse_exp.c
@@ -1,7 +1,20 @@
 
-#include "token.h"
-#include "ast.h"
-#include "stack.h"
+
 #include "parse_exp.h"
 
-parse_exp_init() 
+parse_exp_context_t *parse_exp_init() {
+  parse_exp_context_t *cxt = (parse_exp_context_t *)malloc(sizeof(parse_exp_context_t));
+  if(cxt == NULL) perror(__func__);
+  cxt->stacks[0] = stack_init();
+  cxt->stacks[1] = stack_init();
+  // If the first token is an operator then it must be prefix operator
+  cxt->last_active_stack = 1;
+  return cxt;
+}
+
+void parse_exp_free(parse_exp_context_t *cxt) {
+  stack_free(cxt->stacks[0]);
+  stack_free(cxt->stacks[1]);
+  free(cxt);
+  return;
+}
