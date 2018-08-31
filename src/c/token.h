a7789cdbef84ae05ae30d597124c7bcb3b315908
Ziqi Wang <ziqiw@mit.edu>
2018-08-31 17:12:39 -0400

    Adding token type

--- src/c/token.h
+++ src/c/token.h
@@ -2,12 +2,38 @@
 #ifndef _TOKEN_H
 #define _TOKEN_H
 
+// Types of raw tokens. 
+// This enum type does not distinguish between different expression operators, i.e. both
+// unary "plus" and binary "add" is T_PLUS. Extra information such as operator property 
+// is derived
 typedef enum {
-  
+  // Expression token types
+  T_LPAREN,
+  T_RPAREN,
+  T_LSPAREN,
+  T_RSPAREN,
+  T_DOT,
+  T_ARROW,
+  T_INC,
+  T_DEC,
+  T_PLUS,
+  T_MINUS,
 } token_type_t;
 
+typedef enum {
+  
+} op_property_t;
+
 typedef struct {
-  token_type_t type;
+  // Raw type of the token, dependent only on the string literal
+  token_type_t type;          
+  union {
+    // If the token is part of an expression then this stores the property
+    op_property_t property;
+    // If the token is an identifier, number, char/string literal then this stores
+    // the exact string
+    char *str;
+  };
 } token_t;
 
 #endif
