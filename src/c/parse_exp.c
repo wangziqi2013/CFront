0c8b9f84fc6ca148759754100aee9f80ad91a328
Ziqi Wang <ziqiw@mit.edu>
2018-08-31 18:17:18 -0400

    New tests

--- src/c/parse_exp.c
+++ src/c/parse_exp.c
@@ -1,46 +1,5 @@
 
 #include "token.h"
 #include "ast.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
+#include "stack.h"
 
-#define STACK_INIT_CAPACITY 128
-
-// Implements a general stack which is used in the shift-reduce parsing algo.
-typedef struct {
-  int size;
-  int capacity;
-  void **data;
-} stack_t;
-
-stack_t *init_stack() {
-  stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
-  if(stack == NULL) perror(__func__);
-  stack->data = (void **)malloc(sizeof(void *) * STACK_INIT_CAPACITY);
-  if(stack->data == NULL) perror(__func__);
-  stack->size = 0;
-  stack->capacity = STACK_INIT_CAPACITY;
-
-  return stack;
-}
-
-void stack_push(stack_t *stack, void *p) {
-  if(stack->size == stack->capacity) {
-    void **old = stack->data;
-    stack->data = malloc(sizeof(void *) * stack->capacity * 2);
-    if(stack->data == NULL) perror(__func__);
-    memcpy(stack->data, old, sizeof(void *) * stack->capacity);
-    stack->capacity *= 2;
-    free(old);
-  }
-  assert(stack->size < stack->capacity);
-  stack->data[stack->size++] = p;
-  return;
-}
-
-void *stack_pop(stack_t *stack) {
-  assert(stack->size != 0);
-  return stack->data[--size];
-}
--- src/c/test_parse_exp.c
+++ src/c/test_parse_exp.c
@@ -1,8 +1,32 @@
 
 #include <stdio.h>
+#include <assert.h>
+#include "stack.h"
+
+void test_stack() {
+  printf("=== Test Stack ===\n");
+  stack_t *stack = stack_init();
+
+  for(int i = 0;i < STACK_INIT_CAPACITY * 3 + 123;i++) {
+    stack_push(stack, (void *)i);
+  }
+
+  for(int i = STACK_INIT_CAPACITY * 3 + 123 - 1;i >= 0;i--) {
+    int ret = (int)stack_pop(stack);
+    assert(ret == i);
+  }
+
+  assert(stack->size == 0);
+  assert(stack->capacity > STACK_INIT_CAPACITY * 3);
+
+  stack_free(stack);
+  printf("Pass!\n");
+
+  return;
+}
 
 int main() {
-  printf("Hello World!\n");
-  perror(__func__);
+  printf("=== Hello World! ===\n");
+  test_stack();
   return 0;
 }
