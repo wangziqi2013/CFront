23f8928106557a277697fa394ce15211ecd604b6
Ziqi Wang <ziqiw@mit.edu>
2018-09-01 14:37:33 -0400

    Adding error_row_col_exit

--- src/c/error.h
+++ src/c/error.h
@@ -6,6 +6,13 @@
 #include <stdio.h>
 
 #define error_exit(fmt, ...) do { fprintf(stderr, "Error: " fmt, ##__VA_ARGS__); exit(1); } while(0);
+#define error_row_col_exit(s, fmt, ...) do { \
+                                          int row, col; error_get_row_col(s, &row, &col); \
+                                          fprintf(stderr, "Error (row %d col %d): " fmt, row, col, ##__VA_ARGS__); \
+                                          exit(1); } while(0);
+
+void error_init(const char *s);
+void error_free();
 
 #endif
 
