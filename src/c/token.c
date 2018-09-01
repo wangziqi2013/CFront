53f18ba48bb2872f6f9011397a508fbac8fd1a94
Ziqi Wang <ziqiw@mit.edu>
2018-08-31 20:29:26 -0400

    Adding token_typestr

--- src/c/token.c
+++ src/c/token.c
@@ -4,7 +4,51 @@
 // Converts the token type to a string
 const char *token_typestr(token_type_t type) {
   switch(type) {
-
+    case T_LPAREN: return "T_LPAREN";
+    case T_RPAREN: return "T_RPAREN";
+    case T_LSPAREN: return "T_LSPAREN";
+    case T_RSPAREN: return "T_RSPAREN";
+    case T_DOT: return "T_DOT";
+    case T_ARROW: return "T_ARROW";
+    case T_INC: return "T_INC";
+    case T_DEC: return "T_DEC";
+    case T_PLUS: return "T_PLUS";
+    case T_MINUS: return "T_MINUS";
+    case T_LOGICAL_NOT: return "T_LOGICAL_NOT";
+    case T_BIT_NOT: return "T_BIT_NOT";
+    case T_STAR: return "T_STAR";
+    case T_AND: return "T_AND";
+    case T_SIZEOF: return "T_SIZEOF";
+    case T_DIV: return "T_DIV";
+    case T_MOD: return "T_MOD";
+    case T_LSHIFT: return "T_LSHIFT";
+    case T_RSHIFT: return "T_RSHIFT";
+    case T_LESS: return "T_LESS";
+    case T_GREATER: return "T_GREATER";
+    case T_LEQ: return "T_LEQ";
+    case T_GEQ: return "T_GEQ";
+    case T_EQ: return "T_EQ";
+    case T_NEQ: return "T_NEQ";
+    case T_BIT_XOR: return "T_BIT_XOR";
+    case T_BIT_OR: return "T_BIT_OR";
+    case T_LOGICAL_AND: return "T_RPAREN";
+    case T_LOGICAL_OR: return "T_RPAREN";
+    case T_QMARK: return "T_QMARK";
+    case T_COLON: return "T_COLON";
+    case T_ASSIGN: return "T_ASSIGN";
+    case T_PLUS_ASSIGN: return "T_RPAT_PLUS_ASSIGNREN";
+    case T_MINUS_ASSIGN: return "T_MINUS_ASSIGN";
+    case T_MUL_ASSIGN: return "T_MUL_ASSIGN";
+    case T_DIV_ASSIGN: return "T_DIV_ASSIGN";
+    case T_MOD_ASSIGN: return "T_MOD_ASSIGN";
+    case T_LSHIFT_ASSIGN: return "T_LSHIFT_ASSIGN";
+    case T_RSHIFT_ASSIGN: return "T_RSHIFT_ASSIGN";
+    case T_AND_ASSIGN: return "T_AND_ASSIGN";
+    case T_OR_ASSIGN: return "T_OR_ASSIGN";
+    case T_XOR_ASSIGN: return "T_XOR_ASSIGN";
+    case T_COMMA: return "T_COMMA";
+    case T_LCPAREN: return "T_LCPAREN";
+    case T_RCPAREN: return "T_RCPAREN";
   }
 
   return NULL;
