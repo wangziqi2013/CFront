1aa1d771614e55cb82ec198ffe273d39751ea8c6
Ziqi Wang <ziqiw@mit.edu>
2018-09-01 14:37:28 -0400

    Adding error_get_row_col

--- src/c/error.c
+++ src/c/error.c
@@ -1,3 +1,18 @@
 
 #include "error.h"
 
+// This global pointer holds the begin of the text. We use this pointer and 
+// a given pointer to compute the line and column number
+static const char *begin = NULL;
+
+void error_init(const char *s) { begin = s; }
+void error_free() {}
+
+// Returns the row and column of a given pointer
+void error_get_row_col(const char *s, int *row, int *col) {
+  *row = *col = 1;
+  for(const char *p = begin; p != s && *p != '\0';p++) {
+    if(*p == '\n') (*row)++, *col = 1;
+    else (*col)++;
+  return;
+}
