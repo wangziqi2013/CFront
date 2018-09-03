3377a36e39339767cf842ac7a7f65ea2f32edc1c
Ziqi Wang <ziqiw@mit.edu>
2018-09-03 13:16:12 -0400

    Adding ht_init

--- src/c/hashtable.c
+++ src/c/hashtable.c
@@ -1,3 +1,22 @@
 
 #include "hashtable.h"
 
+hashtable_t *ht_init(cb_t eq) {
+  hashtable_t *ht = (hashtable_t *)malloc(sizeof(hashtable_t));
+  if(ht == NULL) perror(__func__);
+  ht->eq = eq;
+  ht->size = 0;
+  ht->capacity = HT_INIT_CAPACITY;
+  ht->keys = (void **)malloc(sizeof(void *) * HT_INIT_CAPACITY);
+  ht->values = (void **)malloc(sizeof(void *) * HT_INIT_CAPACITY);
+  if(ht->keys == NULL || ht->values == NULL) perror(__func__);
+  memset(ht->keys, 0x00, sizeof(void *) * ht->capacity);
+  return ht;
+}
+
+void ht_free(hashtable_t *ht) {
+  free(ht->keys);
+  free(ht->values);
+  free(ht);
+  return;
+}
--- src/c/hashtable.h
+++ src/c/hashtable.h
@@ -2,12 +2,15 @@
 #ifndef _HASHTABLE_H
 #define _HASHTABLE_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define HT_INIT_CAPACITY 128
+typedef int (*cb_t)(void *, void *);
 
 typedef struct {
-  int (*eq)(void *, void *);
+  cb_t eq;
   int size;
   int capacity;
   void **keys;
