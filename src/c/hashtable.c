4878ea1e0559717533ac246ed166d18394d091b0
Ziqi Wang <ziqiw@mit.edu>
2018-09-03 13:36:18 -0400

    Adding ht_find_slot

--- src/c/hashtable.c
+++ src/c/hashtable.c
@@ -1,10 +1,12 @@
 
 #include "hashtable.h"
 
-hashtable_t *ht_init(cb_t eq) {
+hashtable_t *ht_init(eq_cb_t eq, hash_cb_t hash) {
   hashtable_t *ht = (hashtable_t *)malloc(sizeof(hashtable_t));
   if(ht == NULL) perror(__func__);
   ht->eq = eq;
+  ht->hash = hash;
+  ht->mask = HT_INIT_MASK;
   ht->size = 0;
   ht->capacity = HT_INIT_CAPACITY;
   ht->keys = (void **)malloc(sizeof(void *) * HT_INIT_CAPACITY);
@@ -19,4 +21,21 @@ void ht_free(hashtable_t *ht) {
   free(ht->values);
   free(ht);
   return;
+}
+
+// Returns an existing slot for key, if it already exists, or an empty one
+int ht_find_slot(void **keys, void *key, hashval_t mask) {
+  hashval_t begin = hash(key) & mask;
+  while(keys[begin] && !eq(keys[begin], key)) begin = (begin + 1) & mask;
+  return begin;
+}
+
+void ht_resize(hashtable_t *ht) {
+  assert(ht->size < ht->capacity);
+  ht->capacity *= 2;
+  ht->mask |= (ht->mask << 1);
+  void **new_keys = (void **)malloc(sizeof(void *) * ht->capacity);
+  void **new_values = (void **)malloc(sizeof(void *) * ht->capacity);
+  if(new_keys == NULL || new_values == NULL) perror(__func__);
+
 }
--- src/c/hashtable.h
+++ src/c/hashtable.h
@@ -5,16 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// Must be a power of two
 #define HT_INIT_CAPACITY 128
-typedef int (*cb_t)(void *, void *);
+#define HT_INIT_MASK 0x7F
+
+typedef unsigned long hashval_t;
+typedef int (*eq_cb_t)(void *, void *);
+typedef hashval_t (*hash_cb_t)(void *);
 
 typedef struct {
-  cb_t eq;
+  eq_cb_t eq;
+  hash_cb_t hash;
+  hashval_t mask;
   int size;
   int capacity;
   void **keys;
   void **values;
 } hashtable_t;
+
+hashtable_t *ht_init(eq_cb_t eq, hash_cb_t hash);
+void ht_free(hashtable_t *ht);
+int ht_find_slot(void **keys, void *key, hashval_t mask);
 
 #endif
