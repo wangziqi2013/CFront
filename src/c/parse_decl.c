364768b52081cd68b7baf782bed5f7f5f16b3eba
Ziqi Wang <ziqiw@mit.edu>
2018-09-04 13:18:22 -0400

    Pass compilation

--- src/c/parse_decl.c
+++ src/c/parse_decl.c
@@ -3,40 +3,28 @@
 #include "parse_exp.h"
 #include "parse_decl.h"
 
-parse_decl_cxt_t *parse_decl_init(char *input) {
-  return parse_exp_init(input);
-}
+parse_decl_cxt_t *parse_decl_init(char *input) { return parse_exp_init(input); }
+void parse_decl_free(parse_decl_cxt_t *cxt) { parse_exp_free(cxt); }
 
-void parse_decl_free(parse_decl_cxt_t *cxt) {
-  parse_exp_free(cxt);
-}
-
-// Whether the next token is decl. Note that this is context dependent, and thus 
-// makes C syntex not context-free. Need to check typedef table
-// Note: The following tokens are considered as part of a type expression:
-//   1. ( ) [ ] *  2. specifiers, qualifiers and types 3. typedef'ed names
-// struct, union and enum are recognized here, but they need another parser
-int parse_decl_isdecl(parse_decl_cxt_t *cxt, token_t *token) {
-  token_type_t type = token->type;
-  
-  else if(token->type & DECL_KWD_MASK) return 1;
-  else if(token->type == T_IDENT && ht_find(cxt->udef_types, token->str) != HT_NOTFOUND) return 1;
-  switch(token->type) {
-    case T_LPAREN: case T_RPAREN: case T_STAR: case T_LSPAREN: case T_RSPAREN: return 1;
-  }
+// Whether the token could start a declaration, i.e. being a type or modifier
+int parse_decl_istype(parse_decl_cxt_t *cxt, token_t *token) {
+  if(token->decl_prop & DECL_KWD_MASK) return 1; // built-in type & modifier
+  else if(token->type == T_IDENT && ht_find(cxt->udef_types, token->str) != HT_NOTFOUND) return 1; // udef types
   return 0;
 }
 
 // Same rule as parse_exp_next_token()
+// Note: The following tokens are considered as part of a type expression:
+//   1. ( ) [ ] *  2. specifiers, qualifiers and types 3. typedef'ed names
 token_t *parse_decl_next_token(parse_decl_cxt_t *cxt) {
   token_t *token = token_alloc();
   char *before = cxt->s;
   int valid = 1;
   cxt->s = token_get_next(cxt->s, token);
   if(cxt->s == NULL || 
-     (parse_exp_isempty(cxt, OP_STACK) && (type == T_RPAREN || type == T_RSPAREN))) {
+     (parse_exp_isempty(cxt, OP_STACK) && (token->type == T_RPAREN || token->type == T_RSPAREN)))
     valid = 0;
-  } else {
+  else
     switch(token->type) {
       case T_LPAREN:   // The only symbol that can have two meanings
         token->type = cxt->last_active_stack == OP_STACK ? EXP_LPAREN : EXP_FUNC_CALL; break;
@@ -46,19 +34,17 @@ token_t *parse_decl_next_token(parse_decl_cxt_t *cxt) {
       case T_RSPAREN: token->type = EXP_RSPAREN; break;
       case T_IDENT: // Identifiers are allowed but udef types must be marked as types
         if(ht_find(cxt->udef_types, token->str) != HT_NOTFOUND) { 
-          token->type = T_UDEF; token->decl_prop = DECL_UDEF;
+          token->type = T_UDEF; token->decl_prop |= DECL_UDEF;
         } 
         break;
-      default: valid = 0; break;
+      default: // For keywords and other symbols. Only allow DECL keywords (see token.h)
+        if(!(token->decl_prop & DECL_KWD_MASK)) valid = 0;
     }
-  }
-
   if(!valid) {
     cxt->s = before;
     token_free(token);
     return NULL;
   }
-  
   return token;
 }
 
@@ -79,7 +65,7 @@ token_t *parse_decl(parse_decl_cxt_t *cxt) {
     token_t *top = stack_peek(cxt->stacks[OP_STACK]);
     switch(top->type) {
       case T_DECL: { 
-        if(token->type == T)
+        //if(token->type == T)
       }
       case EXP_DEREF: break;
     }
