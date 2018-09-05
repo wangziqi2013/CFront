885685f6b2eac99d48b062ace6aabe96b4b2014d
Ziqi Wang <ziqiw@mit.edu>
2018-09-04 23:31:02 -0400

    Adding queue routine

--- src/c/queue.c
+++ src/c/queue.c
@@ -1,2 +1,19 @@
 
 #include "queue.h"
+
+queue_t *queue_init() {
+  queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
+  if(queue == NULL) perror(__func__);
+  queue->enq = queue->deq = NULL;
+  queue->size = 0;
+  return queue;
+}
+
+void queue_free(queue_t *queue) {
+  while(queue->deq != NULL) {
+    queue->enq = queue->deq->next;
+    free(deq);
+    queue->deq = queue->enq;
+  }
+  free(queue);
+}
--- src/c/queue.h
+++ src/c/queue.h
@@ -2,11 +2,22 @@
 #ifndef _QUEUE_H
 #define _QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct queue_node_t {
+  void *p;
+  struct queue_node_t *next;
+} queue_node_t;
+
 typedef struct queue_t {
   // Pointer points from deq to enq
-  struct queue_t *enq;
-  struct queue_t *deq;
+  queue_node_t *enq;
+  queue_node_t *deq;
   int size;
 } queue_t;
+
+queue_t *queue_init();
+void queue_free(queue_t *queue);
 
 #endif
