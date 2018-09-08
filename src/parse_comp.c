eadb114f2695ffb428900d24cb80514455658e8a
Ziqi Wang <ziqiw@mit.edu>
2018-09-07 22:14:06 -0400

    Adding parse_struct_union

--- src/parse_comp.c
+++ src/parse_comp.c
@@ -1,6 +1,8 @@
 
 #include "parse_comp.h"
 
+static char STR_UNEXPECTED_END[] = "Unexpected end of file in struct/union declaration"; // Common error message
+
 parse_decl_cxt_t *parse_comp_init(char *input) { return parse_exp_init(input); }
 void parse_comp_free(parse_comp_cxt_t *cxt) { parse_exp_free(cxt); }
 
@@ -13,4 +15,38 @@ token_t *parse_comp(parse_exp_cxt_t *cxt) {
     case T_ENUM: return parse_enum(cxt, token);
     default: assert(0);
   }
+}
+
+token_t *parse_struct_union(parse_comp_cxt_t *cxt, token_t *root) {
+  token_t *name = token_lookahead_notnull(cxt->token_cxt, 1);
+  // Name is either identifier or empty as field child
+  ast_append_child(root, name->type == T_IDENT ? token_get_next(cxt->token_cxt) : token_get_empty());
+  if(!token_consume_type(cxt->token_cxt, T_LCPAREN)) 
+    error_row_col_exit(root->offset, "Expecting identifier or \'{\' after struct/union\n");
+  while(1) { // loop on lines
+    if(token_lookahead_notnull(cxt->token_cxt, 1)->type == T_RCPAREN) break; // Finish parsing on '}'
+    token_t *comp_decl = ast_append_child(token_alloc_type(T_COMP_DECL), parse_basetype(cxt));
+    while(1) { // loop on fields
+      token_t *field = token_alloc(T_COMP_FIELD);
+      ast_append_child(comp_decl, field);
+      token_t *decl = parse_decl(cxt, PARSE_DECL_NOBASETYPE);
+      ast_append_child(field, decl); // Declarator body, can be named or unamed
+      token_t *la = token_lookahead_notnull(cxt->token_cxt, 1);
+      else if(la->type == T_COLON) {
+        token_consume_type(cxt->token_cxt, T_COLON);
+        ast_append_child(field, parse_exp(cxt, PARSE_EXP_NOCOMMA));
+        la = token_lookahead_notnull(cxt->token_cxt, 1);
+      }
+      if(la->type == T_COMMA) { token_consume_type(cxt->token_cxt, T_COMMA); }
+      else if(la->type == T_SEMICOLON) { break; } // Finish parsing the field on ';'
+      else { error_row_col_exit(la->offset, "Unexpected symbol \"%s\" in struct/union field declaration\n", 
+                                token_typestr(la->type)); }
+    }
+  }
+  return NULL;
+}
+
+token_t *parse_enum(parse_comp_cxt_t *cxt, token_t *root) {
+  (void)cxt; (void)root;
+  return NULL;
 }
