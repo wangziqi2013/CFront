725c0bc9aac2f5a5549006b030d4dd137e0a46ba
Ziqi Wang <ziqiw@mit.edu>
2018-09-08 20:25:11 -0400

    Factor out common code

--- src/parse_comp.h
+++ src/parse_comp.h
@@ -9,6 +9,7 @@ typedef parse_exp_cxt_t parse_comp_cxt_t;
 parse_decl_cxt_t *parse_comp_init(char *input);
 void parse_comp_free(parse_decl_cxt_t *cxt);
 token_t *parse_comp(parse_exp_cxt_t *cxt);
+int parse_name_body(parse_comp_cxt_t *cxt, token_t *root);
 token_t *parse_struct_union(parse_comp_cxt_t *cxt, token_t *root);
 token_t *parse_enum(parse_comp_cxt_t *cxt, token_t *root);
 
