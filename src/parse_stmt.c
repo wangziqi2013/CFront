1444e1fef473f896d5abc21335fd682a0276121c
Ziqi Wang <ziqiw@mit.edu>
2018-09-10 21:06:17 -0400

    Adding basic functions

--- src/parse_stmt.c
+++ src/parse_stmt.c
@@ -1,3 +1,9 @@
 
 #include "parse_stmt.h"
 
+parse_stmt_cxt_t *parse_stmt_init(char *input) { return parse_exp_init(input); }
+void parse_stmt_free(parse_stmt_cxt_t *cxt) { parse_exp_free(cxt); }
+
+token_t *parse_stmt(parse_stmt_cxt_t *cxt) {
+  (void)cxt; return NULL;
+}
--- src/parse_stmt.h
+++ src/parse_stmt.h
@@ -2,6 +2,11 @@
 #ifndef _PARSE_STMT_H
 #define _PARSE_STMT_H
 
+#include "parse_exp.h"
 
+typedef parse_exp_cxt_t parse_stmt_cxt_t;
+
+parse_stmt_cxt_t *parse_stmt_init(char *input);
+void parse_stmt_free(parse_stmt_cxt_t *cxt);
 
 #endif
