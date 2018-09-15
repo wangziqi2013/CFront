3d5930b9f40a102114a6a7259874042816f79a71
Ziqi Wang <ziqiw@mit.edu>
2018-09-15 14:50:44 -0400

    Adding scope_t *scope_init(int level)

--- src/type.c
+++ src/type.c
@@ -2,6 +2,22 @@
 #include "token.h"
 #include "type.h"
 
+scope_t *scope_init(int level) {
+  scope_t *scope = (scope_t *)malloc(sizeof(scope_t));
+  if(scope == NULL) syserror(__func__);
+  scope->level = level;
+  scope->enums = ht_str_init();
+  scope->vars = ht_str_init();
+  scope->structs = ht_str_init();
+  scope->unions = ht_str_init();
+  scope->udefs = ht_str_init();
+  return scope;
+}
+
+void scope_free(scope_t *scope) {
+
+}
+
 type_cxt_t *type_init() {
 
 }
