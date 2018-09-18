c27b5e2fe7f7dfdf0afd013d36cac48b70b20926
Ziqi Wang <ziqiw@mit.edu>
2018-09-17 22:16:59 -0400

    Adding bt_insert()

--- src/bintree.c
+++ src/bintree.c
@@ -11,13 +11,28 @@ btnode_t *btnode_alloc(void *key, void *value) {
 }
 void btnode_free(btnode_t *node) { free(node); }
 
-bintree_t *bt_alloc(cmp_cb_t cmp, eq_cb_t eq) {
+bintree_t *bt_alloc(cmp_cb_t cmp) {
   bintree_t *bt = (bintree_t *)malloc(sizeof(bintree_t));
   if(bt == NULL) syserror(__func__);
-  bt->cmp = cmp, bt->eq = eq;
+  bt->cmp = cmp;
   bt->root = NULL;
   bt->size = 0;
   return bt;
 }
 void bt_free(bintree_t *bt) { free(bt); }
 bintree_t *bt_str_alloc() { return bt_alloc(strcmp_cb, streq_cb); }
+
+// Insert the key, or return an existing key
+btnode_t *bt_insert(bintree_t *bt, void *key, void *value) {
+  btnode_t *found = NULL;
+  bt->root = _bt_insert(bt, bt->root, key, value, &found);
+  return found;
+}
+btnode_t *_bt_insert(bintree_t *bt, btnode_t *node, void *key, void *value, btnode_t **found) {
+  if(node == NULL) { bt->size++; *found = btnode_alloc(key, value); return *found; } // Creates a new node
+  int cmp = bt->cmp(key, node->key);
+  if(cmp == 0) *found = node;
+  else if(cmp < 0) node->left = _bt_insert(bt, node->left, key, value);
+  else node->right = _bt_insert(bt, node->right, key, value);
+  return node;
+}
--- src/bintree.h
+++ src/bintree.h
@@ -10,16 +10,18 @@ typedef struct btnode {
   struct btnode *left, *right;
 } btnode_t;
 
+// The good thing about a binary tree search structure is that the physical size
+// grows proportionally with the logical size, which is desirable for structures
+// that are usually small, but sometimes huge
 typedef struct {
   int size;
   cmp_cb_t cmp;
-  eq_cb_t eq;
   btnode_t *root;
 } bintree_t;
 
 btnode_t *btnode_alloc(void *key, void *value);
 void btnode_free(btnode_t *node);
-bintree_t *bt_alloc(cmp_cb_t cmp, eq_cb_t eq);
+bintree_t *bt_alloc(cmp_cb_t cmp);
 void bt_free(bintree_t *bt);
 bintree_t *bt_str_alloc();
 
