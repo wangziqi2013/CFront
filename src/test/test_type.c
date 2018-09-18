06b81bba83c62f1f18bade8fdf16ad82c9c25164
Ziqi Wang <ziqiw@mit.edu>
2018-09-17 23:21:47 -0400

    Adding test

--- src/test/test_type.c
+++ src/test/test_type.c
@@ -7,6 +7,7 @@
 #include "ast.h"
 #include "parse.h"
 #include "hashtable.h"
+#include "bintree.h"
 #include "type.h"
 
 void test_scope_init() {
@@ -43,9 +44,51 @@ void test_scope_init() {
   return;
 }
 
+void test_bintree() {
+  printf("=== Test Binary Tree ===\n");
+  const int test_size = 128 * 10 + 100;
+  const int test_len = 16;
+  const char alphabet[] = {"qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM_"};
+  for(int seed = 0;seed < 100;seed++) {
+    srand(seed);
+    char **tests = malloc(sizeof(char *) * test_size);
+    void **results = malloc(sizeof(void *) * test_size);
+    for(int i = 0;i < test_size;i++) {
+      tests[i] = malloc(sizeof(char) * test_len);
+      for(int j = 0;j < test_len - 1;j++) tests[i][j] = alphabet[rand() % (sizeof(alphabet) - 1)]; // Do not allow '\0'
+      tests[i][test_len - 1] = '\0';
+    }
+
+    bintree_t *bt = bt_str_init();
+    for(int i = 0;i < test_size;i++) {
+      results[i] = bt_insert(bt, tests[i], tests[i]);
+    }
+    
+    for(int i = test_size - 1;i >= 0;i--) {
+      void *ret = bt_find(bt, tests[i]);
+      assert(ret != HT_NOTFOUND);
+      assert(strcmp(ret, tests[i]) == 0);
+    }
+
+    assert(bt_find(bt, "wangziqi2013") == HT_NOTFOUND);
+    assert(bt_find(bt, "+_1234567890") == HT_NOTFOUND);
+    assert(bt_find(bt, "!@#$") == HT_NOTFOUND);
+    assert(bt_find(bt, "QWERT[]{}") == HT_NOTFOUND);
+
+    for(int i = 0;i < test_size;i++) free(tests[i]);
+    free(tests);
+    free(results);
+    bt_free(bt);
+    printf("Finished: %06d [ Size: %d ]\r", seed, bt->size);
+  }
+  printf("\nPass!\n");
+  return;
+}
+
 int main() {
   printf("=== Hello World! ===\n");
   test_scope_init();
+  test_bintree();
   return 0;
 }
   
