a59ffa4c02914f61715fe2b5a8d6d510d51596f1
Ziqi Wang <ziqiw@mit.edu>
2018-09-20 18:09:45 -0400

    Adding list_init

--- src/list.c
+++ src/list.c
@@ -3,3 +3,11 @@
 #include <stdlib.h>
 #include "error.h"
 #include "list.h"
+
+list_t *list_init() {
+  list_t *list = (list_t *)malloc(sizeof(list_t));
+  if(list == NULL) syserror(__func__);
+  list->size = 0;
+  list->head = NULL;
+  return list;
+}
