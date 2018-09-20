3013a6df8d6c56f8ff9e5c49d69afcb207128045
Ziqi Wang <ziqiw@mit.edu>
2018-09-20 18:09:12 -0400

    Adding type decl

--- src/list.h
+++ src/list.h
@@ -2,5 +2,14 @@
 #ifndef _LIST_H
 #define _LIST_H
 
+typedef struct listnode {
+  void *key, *value;
+  struct listnode *next;
+} listnode_t;
+
+typedef struct {
+  listnode_t *head;
+  int size;
+} list_t;
 
 #endif
