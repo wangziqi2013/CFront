dacf0ab13647b8f73a2b8f064a3a3c3f230fd878
Ziqi Wang <ziqiw@mit.edu>
2018-09-24 22:25:53 -0400

    Working on eval

--- src/eval.c
+++ src/eval.c
@@ -1,6 +1,20 @@
 
+#include <ctype.h>
 #include "eval.h"
 #include "type.h"
+
+// Writes the number of given base into the data area of the token
+void eval_getintimm(value_t *val, token_t *token) {
+  token_type_t type = token->type;
+  char *s = token->s;
+  int base;
+  switch(token->type) {
+    case T_HEX_INT_CONST: s += 2; base = 16; break;
+    case T_OCT_INT_CONST: s++; base = 8; break;
+    case T_DEC_INT_CONST: base = 10; break;
+    default: assert(0);
+  }
+}
 
 value_t *eval_constexpr(token_t *token) {
   return NULL;
