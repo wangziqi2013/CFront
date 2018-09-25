cba08e97edf87b9331d1fe1571e6be995c562c3e
Ziqi Wang <ziqiw@mit.edu>
2018-09-24 22:54:17 -0400

    Adding eval_getintimm

--- src/eval.c
+++ src/eval.c
@@ -5,15 +5,30 @@
 
 // Writes the number of given base into the data area of the token
 void eval_getintimm(value_t *val, token_t *token) {
-  token_type_t type = token->type;
   char *s = token->s;
   int base;
   switch(token->type) {
-    case T_HEX_INT_CONST: s += 2; base = 16; break;
-    case T_OCT_INT_CONST: s++; base = 8; break;
+    case T_HEX_INT_CONST: base = 16; break;
+    case T_OCT_INT_CONST: base = 8; break;
     case T_DEC_INT_CONST: base = 10; break;
     default: assert(0);
   }
+  int sz = type_getintsize(token->decl_prop);  // Number of bytes in the type
+  if(sz > sizeof(type_maxint_t)) 
+    error_row_col_exit(token->offset, "Sorry, do not support integer literal \"%s\" larger than %d bytes: %s\n", 
+                       token->s, sizeof(type_maxint_t));
+  type_maxint_t scratch = (type_maxint_t)0;
+  do { scratch = scratch * base + \
+                 (*s >= 'A' && *s <= 'F') ? (*s - 'A' + 10) : ((*s >= 'a' && *s <= 'f') ? (*s - 'a' + 10) : *s - '0');
+  } while(*++s);
+  switch(sz) {
+    case 1: val->ucharval = (uint8_t)scratch; break;
+    case 2: val->ushortval = (uint16_t)scratch; break;
+    case 4: val->uintval = (uint32_t)scratch; break;
+    case 8: val->ulongval = (uint64_t)scratch; break;
+    default: assert(0); // Should already be detected
+  }
+  return;
 }
 
 value_t *eval_constexpr(token_t *token) {
--- src/eval.h
+++ src/eval.h
@@ -4,7 +4,9 @@
 
 #include "token.h"
 
-struct value_t;
+struct value_t; // Will be included in the .c file
+// Converts a integer literal token into binary value form
+void eval_getintimm(value_t *val, token_t *token);
 value_t *eval_constexpr(token_t *token);
 
 #endif
