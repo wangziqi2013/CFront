8a321c46dcac03d0e535e86ed9b89d6fd860ca85
Ziqi Wang <ziqiw@mit.edu>
2019-05-14 13:50:02 -0400

    Adding eval_const_getintimm

--- src/eval.h
+++ src/eval.h
@@ -6,6 +6,8 @@
 
 struct value_t_struct;
 typedef struct value_t_struct value_t;
+// Converts a constant integer into native int type; no type info
+int eval_const_getintimm(token_t *token);
 // Converts a integer literal token into binary value form
 void eval_getintimm(value_t *val, token_t *token);
 value_t *eval_constexpr(token_t *token);
