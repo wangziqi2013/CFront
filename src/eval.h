ef19e5d42cdacfc5aa2fe3318e4717295cba4c0f
Ziqi Wang <ziqiw@mit.edu>
2019-05-16 19:15:23 -0400

    Adding int_t

--- src/eval.h
+++ src/eval.h
@@ -5,10 +5,14 @@
 #include "token.h"
 #include "ast.h"
 
-struct value_t_struct;
-typedef struct value_t_struct value_t;
+typedef struct {
+  int sign;      // 0 means unsigned, 1 means signed
+  int size;      // Number of bytes
+} int_t;
 
-char *eval_const_atoi_maxbite(char *s, int base, token_t *token, int *ret); // Take a maximum bite
+extern int_t ints[];
+
+char *eval_const_atoi_maxbite(char *s, int base, token_t *token, int *ret); // Take a maximum bite and return the next to read
 int eval_const_atoi(char *s, int base, token_t *token, int max_char); // Given a string and base convert to integer
 char eval_const_char_token(token_t *token); // Convert char literal into a char type var
 // Evaluating const expression using native int types (or convert other types to int)
