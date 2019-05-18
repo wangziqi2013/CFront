44dd116ec195a135e236820d3cdc401eea95389b
Ziqi Wang <ziqiw@mit.edu>
2019-05-18 02:07:34 -0400

    Change list interface

--- src/list.c
+++ src/list.c
@@ -1,15 +1,13 @@
 
 #include "list.h"
 
-list_t *list_init(eq_cb_t eq) {
+list_t *list_init() {
   list_t *list = (list_t *)malloc(sizeof(list_t));
   SYSEXPECT(list != NULL);
   list->size = 0;
   list->head = list->tail = NULL;
-  list->eq = eq;
   return list;
 }
-list_t *list_str_init() { return list_init(streq_cb); }
 
 void list_free(list_t *list) {
   assert(list->head || !list->tail);
@@ -69,16 +67,16 @@ listnode_t *list_insertat(list_t *list, void *key, void *value, int index) {
   return value;
 }
 
-void *list_insert_nodup(list_t *list, void *key, void *value) {
-  void *ret = list_find(list, key);
+void *list_insert_nodup(list_t *list, void *key, void *value, eq_cb_t eq) {
+  void *ret = list_find(list, key, eq);
   if(ret == LIST_NOTFOUND) value = list_insert(list, key, value);
   return ret;
 }
 
 // Search for the given key, and return value; Return LIST_NOTFOUND if not found
-void *list_find(list_t *list, void *key) {
+void *list_find(list_t *list, void *key, eq_cb_t eq) {
   listnode_t *curr = list->head;
-  while(curr) if(list->eq(key, curr->key)) return curr->value; else curr = curr->next;
+  while(curr) if(eq(key, curr->key)) return curr->value; else curr = curr->next;
   return LIST_NOTFOUND;
 }
 
@@ -93,11 +91,11 @@ const listnode_t *list_findat(list_t *list, int index) {
 }
 
 // Removes the key from the list. Return value if key exists; LIST_NOTFOUND otherwise
-void *list_remove(list_t *list, void *key) {
+void *list_remove(list_t *list, void *key, eq_cb_t eq) {
   listnode_t *curr = list->head, *prev = curr;
   if(curr == NULL) return LIST_NOTFOUND;
   void *ret = NULL;
-  if(list->eq(curr->key, key)) {
+  if(eq(curr->key, key)) {
     list->head = curr->next;  // Could be NULL
     ret = curr->value;
     listnode_free(curr);
@@ -107,7 +105,7 @@ void *list_remove(list_t *list, void *key) {
   }
   do {
     curr = curr->next;
-    if(curr && list->eq(curr->key, key)) {
+    if(curr && eq(curr->key, key)) {
       prev->next = curr->next;
       ret = curr->value;
       listnode_free(curr);
--- src/list.h
+++ src/list.h
@@ -14,27 +14,25 @@ typedef struct listnode {
 typedef struct {
   listnode_t *head, *tail;
   int size;
-  eq_cb_t eq;   // Call back function for comparing keys
 } list_t;
 
-inline listnode_t *list_head(list_t *list) { return list->head; }
-inline listnode_t *list_tail(list_t *list) { return list->tail; }
-inline listnode_t *list_next(listnode_t *node) { return node->next; }
-inline void *list_key(listnode_t *node) { return node->key; }
-inline void *list_value(listnode_t *node) { return node->value; }
+static inline listnode_t *list_head(list_t *list) { return list->head; }
+static inline listnode_t *list_tail(list_t *list) { return list->tail; }
+static inline listnode_t *list_next(listnode_t *node) { return node->next; }
+static inline void *list_key(listnode_t *node) { return node->key; }
+static inline void *list_value(listnode_t *node) { return node->value; }
 
-list_t *list_init(eq_cb_t eq);
-list_t *list_str_init();
+list_t *list_init();
 void list_free(list_t *list);
 int list_size(list_t *list);
 listnode_t *listnode_alloc();
 void listnode_free(listnode_t *node);
 void *list_insert(list_t *list, void *key, void *value);
 listnode_t *list_insertat(list_t *list, void *key, void *value, int index);
-void *list_insert_nodup(list_t *list, void *key, void *value);
-void *list_find(list_t *list, void *key);
+void *list_insert_nodup(list_t *list, void *key, void *value, eq_cb_t eq);
+void *list_find(list_t *list, void *key, eq_cb_t eq);
 const listnode_t *list_findat(list_t *list, int index);
-void *list_remove(list_t *list, void *key);
+void *list_remove(list_t *list, void *key, eq_cb_t eq);
 void *list_removeat(list_t *list, int index, void **key);
 
 #endif
