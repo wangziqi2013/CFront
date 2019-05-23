32ca79550682810f412ae1c05bcd410c81ba79ac
Ziqi Wang <ziqiw@mit.edu>
2019-05-23 00:18:15 -0400

    Working on bit fields

--- src/type.c
+++ src/type.c
@@ -449,9 +449,25 @@ comp_t *type_getcomp(type_cxt_t *cxt, token_t *token, int is_forward) {
           }
         } else { list_insert(comp->field_list, NULL, f); } // Anonymous non-comp field, insert
       }
-      if(token->type == T_STRUCT) curr_offset += f->type->size;   // Even true for promoted struct/union
+      if(token->type == T_STRUCT) {
+        // Non-Bit field always increments the offset; This works also for promoted types
+        // Note: If prev is bit field then we have not incremented the offset yet
+        if(f->bitfield_size == -1 && prev_field->bitfield_size == -1) {
+          curr_offset += f->type->size; // Both normal fields; size has been set above
+        } else if(f->bitfield_size == -1 && prev_field->bitfield_size != -1) {
+          curr_offset += (f->type->size + prev_field->type->size) // Increment for both prev and curr
+        } else if(f->bitfield_size != -1 && prev_field->bitfield_size == -1) {
+          f->bitfield_offset = f->bitfield_size; // Starting a bit field (may potentially have more later)
+        } else { // Both are valid bitfields - try to coalesce!
+          if(prev_field->bitfield_offset + f->bitfield_size <= prev_field->size) {
+            f->offset = prev_field->offset;
+            f->bitfield_offset = 
+          }
+        }
+      }
       else if(max_size < f->type->size) max_size = f->type->size;
       field = field->sibling;
+      prev_field = f;
     }
     entry = entry->sibling;
   }
