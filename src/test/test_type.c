55c55df5c76b4e24de396494238d68531862831e
Ziqi Wang <ziqiw@mit.edu>
2019-05-23 19:01:11 -0400

    Adding new test for unnamed enum

--- src/test/test_type.c
+++ src/test/test_type.c
@@ -503,7 +503,7 @@ void test_type_getenum() {
   token_t *token;
   type_t *type;
   str_t *s;
-  char test1[] = "enum enum_name { a = 1, b, c = 10, }";
+  char test1[] = "enum enum_name { a = 1, b, c = 10, }"; // Tests named enum
   parse_cxt = parse_exp_init(test1);
   type_cxt = type_sys_init();
   token = parse_decl(parse_cxt, PARSE_DECL_HASBASETYPE);
@@ -512,6 +512,21 @@ void test_type_getenum() {
   type = type_gettype(type_cxt, token, ast_getchild(token, 0), 0);
   s = type_print(type, NULL, NULL, 1, 0);
   printf("%s\n", test1);
+  printf("%s\n", s->s);
+  str_free(s);
+  type_sys_free(type_cxt);
+  parse_exp_free(parse_cxt);
+  ast_free(token);
+  printf("=====================================\n");
+  char test2[] = "struct stru { enum {a,b,c} x; enum {d,e, /* a -> name clash with previous enum*/} y; }"; // Tests unnamed enum
+  parse_cxt = parse_exp_init(test2);
+  type_cxt = type_sys_init();
+  token = parse_decl(parse_cxt, PARSE_DECL_HASBASETYPE);
+  assert(token_get_next(parse_cxt->token_cxt) == NULL);
+  ast_print(token, 0);
+  type = type_gettype(type_cxt, token, ast_getchild(token, 0), 0);
+  s = type_print(type, NULL, NULL, 1, 0);
+  printf("%s\n", test2);
   printf("%s\n", s->s);
   str_free(s);
   type_sys_free(type_cxt);
