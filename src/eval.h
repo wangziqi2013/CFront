4857f3bc0682a4c7661211c9dfb7dd4136dbb7e1
Ziqi Wang <ziqiw@mit.edu>
2019-06-05 22:12:22 -0400

    Adding int_imm_t

--- src/eval.h
+++ src/eval.h
@@ -11,8 +11,32 @@
 
 // Used as the parameter to eval_const_atoi()
 #define ATOI_NO_CHECK_END 0
-#define ATOI_CHECK_END    1
-#define ATOI_NO_MAX_CHAR  0
+#define ATOI_CHECK_END    1    // Do not report error if there is still char after the int literal
+#define ATOI_NO_MAX_CHAR  0    // For \xhh \ooo we only eat 2 and 3 chars respectively
+
+typedef struct {
+  int size; int sign;
+  union {
+    uint8_t  uint8;
+    int8_t   int8;
+    uint16_t uint16;
+    int16_t  int16;
+    uint32_t uint32;
+    int32_t  int32;
+    uint64_t uint64;
+    int64_t  int64;
+  };
+  union {
+    uint8_t  uint8_hi;
+    int8_t   int8_hi;
+    uint16_t uint16_hi;
+    int16_t  int16_hi;
+    uint32_t uint32_hi;
+    int32_t  int32_hi;
+    uint64_t uint64_hi;
+    int64_t  int64_hi;
+  };
+} int_imm_t; // Immediate value of integer
 
 char *eval_hex_char(char ch);
 str_t *eval_print_const_str(str_t *s);
