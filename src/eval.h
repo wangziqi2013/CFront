631aeaea398131c2e1406faef6e9f81711324265
Ziqi Wang <ziqiw@mit.edu>
2019-06-10 02:46:56 -0400

    Updating prototype

--- src/eval.h
+++ src/eval.h
@@ -21,7 +21,8 @@ uint64_t eval_int_masks[9];
 uint64_t eval_const_get_mask(int op);
 uint64_t eval_const_get_sign_mask(int op);
 value_t *eval_const_adjust_size(type_cxt_t *cxt, value_t *value, int to, int from, int is_signed);
-value_t *eval_const_add(type_cxt_t *cxt, value_t *op1, value_t *op2, int size, int is_signed);
+uint64_t eval_const_add(type_cxt_t *cxt, value_t *op1, value_t *op2, int size, int is_signed, int *overflow);
+uint64_t eval_const_sub(type_cxt_t *cxt, value_t *op1, value_t *op2, int size, int is_signed, int *overflow);
 
 char *eval_hex_char(char ch);
 str_t *eval_print_const_str(str_t *s);
