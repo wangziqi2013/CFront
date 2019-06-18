9d300667a7da3974920bacb00a910ba31e52805f
Ziqi Wang <ziqiw@mit.edu>
2019-06-17 22:04:03 -0400

    Adding cgen_global_data_t

--- src/cgen.h
+++ src/cgen.h
@@ -5,6 +5,12 @@
 #include "type.h"
 #include "ast.h"
 
+typedef struct {
+  void *data;
+  type_t *type;
+  value_t *value;
+} cgen_global_data_t;
+
 void cgen_global_decl(type_cxt_t *cxt, token_t *global_decl);
 void cgen_global_func(type_cxt_t *cxt, token_t *func);
 void cgen(type_cxt_t *cxt, token_t *root);
