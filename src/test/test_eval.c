52a9b8c33fe19715985c7aa8afe40592e35be91d
Ziqi Wang <ziqiw@mit.edu>
2019-06-21 16:41:37 -0400

    Fixing compilation

--- src/test/test_eval.c
+++ src/test/test_eval.c
@@ -91,7 +91,7 @@ void test_eval_const_exp() {
   type_cxt = type_sys_init();
   parse_cxt = parse_exp_init("(1000 + 2 * 3) << 4"); 
   token = parse_exp(parse_cxt, PARSE_EXP_ALLOWALL);
-  ast_print(token, 0);
+  ast_print_(token, 0);
   value = eval_const_exp(type_cxt, token);
   printf("Type: %s Value: 0x%016lX (%ld)\n", type_print_str(0, value->type, NULL, 0), value->uint64, value->int64);
   assert(value->int32 == 16096);
@@ -101,7 +101,7 @@ void test_eval_const_exp() {
   type_cxt = type_sys_init();
   parse_cxt = parse_exp_init("((char)1000 + 2ul * 3) << 4"); 
   token = parse_exp(parse_cxt, PARSE_EXP_ALLOWALL);
-  ast_print(token, 0);
+  ast_print_(token, 0);
   value = eval_const_exp(type_cxt, token);
   printf("Type: %s Value: 0x%016lX (%ld)\n", type_print_str(0, value->type, NULL, 0), value->uint64, value->int64);
   assert(value->int64 == -288); // Because of the sign extension of char type
@@ -111,7 +111,7 @@ void test_eval_const_exp() {
   type_cxt = type_sys_init();
   parse_cxt = parse_exp_init("(signed long)((long)(unsigned long *)(long)(unsigned long *)(long)100 + 2)"); 
   token = parse_exp(parse_cxt, PARSE_EXP_ALLOWALL);
-  ast_print(token, 0);
+  ast_print_(token, 0);
   value = eval_const_exp(type_cxt, token);
   printf("Type: %s Value: 0x%016lX (%ld)\n", type_print_str(0, value->type, NULL, 0), value->uint64, value->int64);
   assert(value->int64 == 102); // Because of the sign extension of char type
