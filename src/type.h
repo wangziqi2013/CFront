638b44c296f6de6e5e009b7c54bb2dca40f77d4c
Ziqi Wang <ziqiw@mit.edu>
2019-06-22 20:19:04 -0400

    Adding shorthand functions

--- src/type.h
+++ src/type.h
@@ -316,7 +316,12 @@ type_t *type_int_convert(type_t *lhs, type_t *rhs);
 int type_cmp(type_t *to, type_t *from);
 int type_cast(type_t *to, type_t *from, int cast_type, char *offset);
 type_t *type_int_promo(type_cxt_t *cxt, type_t *type);
-type_t *type_typeof_op(type_cxt_t *cxt, token_type_t type, type_t *op1, type_t *op2, type_t *op3);
+
+type_t *type_typeof_op_func(type_cxt_t *cxt, token_type_t op, token_t *arg);
+type_t *type_typeof_op_1(type_cxt_t *cxt, token_type_t op, type_t *op1);
+type_t *type_typeof_op_2(type_cxt_t *cxt, token_type_t op, type_t *op1, type_t *op2);
+type_t *type_typeof_op_3(type_cxt_t *cxt, token_type_t op, type_t *op1, type_t *op2, type_t *op3);
+type_t *type_typeof_op(type_cxt_t *cxt, token_type_t op, type_t *op1, type_t *op2, type_t *op3, token_t *arg);
 type_t *type_typeof(type_cxt_t *cxt, token_t *exp, uint32_t options); // Evaluate the type of an expression
 
 #endif
