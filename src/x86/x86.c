0989c5015c38b4f9d6c96a3a337860e90e4b40f5
Ziqi Wang <675973885@qq.com>
2021-05-21 21:43:02 -0400

    Adding prefix_to_flag MMX  version

--- src/x86/x86.c
+++ src/x86/x86.c
@@ -1,3 +1,19 @@
 
-
-
+uint32_t prefix_to_flag(uint8_t byte) {
+  // Load the mask value
+  __m64 mask =_m_from_int64(PREFIX_MMX_MASK);
+  // Broadcast input byte
+  __m64 input = _mm_set1_pi8(byte);
+  // Compare byte-by-byte, 0x00 if equal, 0xFF otherwise
+  __m64 _cmp = _mm_cmpeq_pi8(mask, input);
+  uint64_t cmp = _mm_cvtm64_si64(_cmp);
+  if(cmp == 0UL) {
+    return FLAG_NONE;
+  }
+  // Returns 1 + first index of "1"
+  int index = __builtin_ffs(cmp) - 1;
+  assert(index != -1);
+  assert(index % 8 == 0);
+  index = (index >> 3) + 1;
+  return (uint32_t)index;
+}
