23a628ab74c9798767ab088af3a7cc237df4a20e
Ziqi Wang <675973885@qq.com>
2021-05-21 21:12:54 -0400

    Adding stubs

---
+++ src/x86/x86.h
@@ -0,0 +1,43 @@
+
+#ifndef _X86_H
+#define _X86_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <limits.h>
+#include <string.h>
+#include <error.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <immintrin.h>
+#include <assert.h>
+
+// Error reporting and system call assertion
+#define SYSEXPECT(expr) do { if(!(expr)) { perror(__func__); assert(0); exit(1); } } while(0)
+#define error_exit(fmt, ...) do { fprintf(stderr, "%s error: " fmt, __func__, ##__VA_ARGS__); assert(0); exit(1); } while(0);
+#ifndef NDEBUG
+#define dbg_printf(fmt, ...) do { fprintf(stderr, fmt, ##__VA_ARGS__); } while(0);
+#else
+#define dbg_printf(fmt, ...) do {} while(0);
+#endif
+
+// Branching macro (this may have already been defined in other source files)
+#ifndef likely
+#define likely(x)       __builtin_expect((x),1)
+#endif
+#ifndef unlikely
+#define unlikely(x)     __builtin_expect((x),0)
+#endif
+
+// Testing function print name and pass
+#define TEST_BEGIN() do { printf("========== %s ==========\n", __func__); } while(0);
+#define TEST_PASS() do { printf("Pass!\n"); } while(0);
+
+#endif
